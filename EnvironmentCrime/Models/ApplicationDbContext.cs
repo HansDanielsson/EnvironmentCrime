113186e92523edfe4b7a6b0ebcae92734b9b611b
Hans Danielsson <Hans.Ro.Danielsson@gmail.com>
2025-10-01 08:31:05 +0200

    Files Init

---
+++ EnvironmentCrime/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EnvironmentCrime.Models
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+		public DbSet<Errand> Errands { get; set; }
+		public DbSet<ErrandStatus> ErrandStatuses { get; set; }
+		public DbSet<Employee> Employees { get; set; }
+		public DbSet<Department> Departments { get; set; }
+		public DbSet<Sequence> Sequences { get; set; }
+	}
+}
---
+++ EnvironmentCrime/Models/EFRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace EnvironmentCrime.Models
+{
+	public class EFRepository : IERepository
+	{
+		private ApplicationDbContext context;
+		public EFRepository(ApplicationDbContext ctx)
+		{
+			context = ctx;
+		}
+		public IQueryable<Errand> Errands => context.Errands;
+		public IQueryable<ErrandStatus> ErrandStatuses => context.ErrandStatuses;
+
+	}
+}
